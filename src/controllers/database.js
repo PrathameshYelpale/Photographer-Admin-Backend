47f30cf755830ac7ef906f53f456e5a501cd641b
Prathamesh Yelpale <prathmesh735@gmail.com>
2025-01-15 21:16:10 +0530

    database changed

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://dhanajiadate:3x9Zy962kRiMDUM4@photographers-db.emkps.mongodb.net/?retryWrites=true&w=majority&appName=Photographers-db";
+// const uri = "mongodb+srv://dhanajiadate:3x9Zy962kRiMDUM4@photographers-db.emkps.mongodb.net/?retryWrites=true&w=majority&appName=Photographers-db";
+const uri = "mongodb+srv://dhanajiadate:3x9Zy962kRiMDUM4@photographers-db.emkps.mongodb.net/Photographers-Admin_DB?retryWrites=true&w=majority&appName=Photographers-db";
+
 const connectDB = async () => {
     await mongoose.connect(uri)
     console.log("DB connected successfullly !");
